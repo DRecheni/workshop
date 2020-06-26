30c87126b88a30a3b148527d035dd8980b186ff0
davidrecheni <recheni@facturee.de>
2020-06-26 10:03:33 +0200

    Rollback init

---
+++ conf/conf.js
@@ -0,0 +1,10 @@
+module.exports = {
+    db : "dev" ,
+    CLIENT_TOKEN_CONTACTS : '{"access_token":"ya29.GlvKBrWdzVwKPrvPmHuYUcrKTXGpzLLw8NK2REnyh839-AUpD0qGh71M1zvMNg2kgS42H5_MYdXSjix3e0mUtk4z_2BsQ4ayEiJg-QE96BqXu1Zd6z0YeM4NN-vX","refresh_token":"1/5DUGQShT1Xe_uUc4Vqg8pETY8yU2ExrkDRp7fbyBFzf2gOW-hitTcbVcaT-SSAEM","scope":"https://www.googleapis.com/auth/contacts","token_type":"Bearer","expiry_date":1552392993347}',
+    APP_TOKEN : '{"installed":{"client_id":"234139906119-85u6dm1sm70g41m6feijq5ejfqds4sv5.apps.googleusercontent.com","project_id":"fadash","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"0rYKuVCzSd0elcmElGiDna6k","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}',
+    AUTOMATIC_MAIL_TOKEN : '{"access_token":"ya29.GlveBpICoCX10KyFJGWIpGY5tVsGsXTbebYAUz6Q9IvqNUwahCHXPoWMwc7lesoL2uZd7pN0TY-Xrfl5BK9hnVOCbdsNds8IEcM9fKXkPaU58e4YCq6CoH2QSyew","refresh_token":"1/S4Bc9mFn21wBZQySOZeqxgwsh0bjEm3xJ6dHds_o7zE","scope":"https://www.googleapis.com/auth/gmail.modify","token_type":"Bearer","expiry_date":1554119363684}',
+    CREDIT_SAFE_CREDENTIALS : '{ "username" : "walzel@facturee.de" , "password" : "cBM812M$5-$()6n#6_8-7*"  }',
+    VAT_VALIDATOR_KEY : 'e70af1b5ce7f49c290f561a811dfda20',
+    EXTERNAL_URL_BASE : 'http://0.0.0.0:',
+    SEND_OFFER_MAIL_DAYS_AGO : 0
+}
